465a45e2dc175e26ebe79726b545c59d5674cbed
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-22 00:06:58 +0500

    Create Exercise2.py

---
+++ Career Tracks/Python Programmer/Data Types for Data Science in Python/Dictionaries - the root of Python/Exercise2.py
@@ -0,0 +1,14 @@
+"""
+1> Safely print rank 7 from the names dictionary.
+2> Safely print the type of rank 100 from the names dictionary.
+3> Safely print rank 105 from the names dictionary or 'Not Found' if 105 is not found.
+"""
+
+# Safely print rank 7 from the names dictionary
+print(names.get(7))
+
+# Safely print the type of rank 100 from the names dictionary
+print(type(names.get(100)))
+
+# Safely print rank 105 from the names dictionary or 'Not Found'
+print(names.get(105,'Not Found'))
