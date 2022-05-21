bca42d45b8bf69df6e7400cf5214c144983a5f3e
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-22 01:02:45 +0500

    Create Exercise3.py

---
+++ Career Tracks/Python Programmer/Data Types for Data Science in Python/Meet the collections module/Exercise3.py
@@ -0,0 +1,21 @@
+"""
+1> Create an empty dictionary called ridership.
+2> Iterate over entries, unpacking it into the variables date, stop, and riders.
+3> Check to see if the date already exists in the ridership dictionary. If it does not exist, create an empty list for the date key.
+4> Append a tuple consisting of stop and riders to the date key of the ridership dictionary.
+5> Print the ridership for '03/09/2016'.
+"""
+# Create an empty dictionary: ridership
+ridership = {}
+
+# Iterate over the entries
+for date,stop,riders in entries:
+    # Check to see if date is already in the ridership dictionary
+    if date not in ridership:
+        # Create an empty list for any missing date
+        ridership[date] = []
+    # Append the stop and riders as a tuple to the date keys list
+    ridership[date].append((stop,riders))
+    
+# Print the ridership for '03/09/2016'
+print(ridership['03/09/2016'])
