4fd7d4d8052776a692aa0fdac84e3ca758e54ee8
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-22 00:26:05 +0500

    Create Exercise7.py

---
+++ Career Tracks/Python Programmer/Data Types for Data Science in Python/Dictionaries - the root of Python/Exercise7.py
@@ -0,0 +1,26 @@
+"""
+1> Check to see if 2011 is in the baby_names dictionary.
+    Print 'Found 2011' if it is present.
+2> Check to see if 1 is in baby_names[2012].
+    Print 'Found Rank 1 in 2012' if found and 'Rank 1 missing from 2012' if not found.
+3> Check to see if rank 5 is in baby_names[2013].
+    Print 'Found Rank 5' if it is present.
+"""
+
+# Check to see if 2011 is in baby_names
+if 2011 in baby_names:
+    # Print 'Found 2011'
+    print('Found 2011')
+    
+# Check to see if rank 1 is in 2012
+if 1 in baby_names[2012]:
+    # Print 'Found Rank 1 in 2012' if found
+    print('Found Rank 1 in 2012')
+else:
+    # Print 'Rank 1 missing from 2012' if not found
+    print('Rank 1 missing from 2012')
+    
+# Check to see if Rank 5 is in 2013
+if 5 in baby_names[2013]:
+   # Print 'Found Rank 5'
+   print('Found Rank 5')
