88f879e869bfef7a67c553e529ac294f42e56cec
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-21 22:40:21 +0500

    Create Exercise4.py

---
+++ Career Tracks/Python Programmer/Data Types for Data Science in Python/Fundamental data types/Exercise4.py
@@ -0,0 +1,14 @@
+"""
+1> Create a variable named normal and set it equal to 'simple'.
+2> Create a variable named error and set it equal 'trailing comma',.
+3> Print the type of the normal and error variables.
+"""
+# Create the normal variable: normal
+normal = 'simple'
+
+# Create the mistaken variable: error
+error = 'trailing comma',
+
+# Print the types of the variables
+print(type(normal))
+print(type(error))
