0663d987cff1f4512d55381e1e5a0cd5ac50cc67
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-22 10:26:34 +0500

    Create Exercise4.py

---
+++ Career Tracks/Python Programmer/Data Types for Data Science in Python/Meet the collections module/Exercise4.py
@@ -0,0 +1,20 @@
+"""
+1> Import defaultdict from collections.
+2> Create a defaultdict with a default type of list called ridership.
+3> Iterate over the list entries, unpacking it into the variables date, stop, and riders, exactly as you did in the previous exercise.
+4> Use stop as the key of the ridership dictionary and append riders to its value.
+5> Print the first 10 items of the ridership dictionary. You can use the .items() method for this. Remember, you have to convert ridership.items() to a list before slicing.
+"""
+# Import defaultdict
+from collections import defaultdict
+
+# Create a defaultdict with a default type of list: ridership
+ridership = defaultdict(list)
+
+# Iterate over the entries
+for date, stop, riders in entries:
+    # Use the stop as the key of ridership and append the riders to its value
+    ridership[stop].append(riders)
+    
+# Print the first 10 items of the ridership dictionary
+print(list(ridership.items())[:10])
