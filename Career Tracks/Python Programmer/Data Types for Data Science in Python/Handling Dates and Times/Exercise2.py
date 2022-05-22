c74350482995a07cf07bf20eaf648c3ffc51a257
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-22 11:30:18 +0500

    Create Exercise2.py

---
+++ Career Tracks/Python Programmer/Data Types for Data Science in Python/Handling Dates and Times/Exercise2.py
@@ -0,0 +1,12 @@
+"""
+1> Loop over the first 10 items of the datetimes_list, using item as your iterator variable.
+2> Print out the item as a string in the format of 'MM/DD/YYYY'. For this, the format string is '%m/%d/%Y'.
+3> Print out the item as an ISO standard string.
+"""
+# Loop over the first 10 items of the datetimes_list
+for item in datetimes_list[:10]:
+    # Print out the record as a string in the format of 'MM/DD/YYYY'
+    print(item.strftime('%m/%d/%Y'))
+    
+    # Print out the record as an ISO standard string
+    print(item.isoformat())
