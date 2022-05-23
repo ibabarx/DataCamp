9db6b3963e95bd972c7ae02ce337238ecfb8b2ee
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-23 14:32:14 +0500

    Create Exercise3.py

---
+++ Career Tracks/Python Programmer/Data Types for Data Science in Python/Answering Data Science Questions/Exercise3.py
@@ -0,0 +1,27 @@
+"""
+1> Import defaultdict from collections and datetime from datetime.
+2> Create a dictionary that defaults to a list called locations_by_month.
+3> Loop over the crime_data list:
+4> Convert the first element to a date object exactly like you did in the previous exercise.
+5> If the year is 2016, set the key of locations_by_month to be the month of date and .append() the location (fifth element of row) to the values list.
+6> Print the dictionary. This has been done for you, so hit 'Submit Answer' to see the result!
+"""
+# Import necessary modules
+from collections import defaultdict
+from datetime import datetime
+
+# Create a dictionary that defaults to a list: locations_by_month
+locations_by_month = defaultdict(list)
+
+# Loop over the crime_data list
+for row in crime_data:
+    # Convert the first element to a date object
+    date = datetime.strptime(row[0], '%m/%d/%Y %I:%M:%S %p')
+    
+    # If the year is 2016 
+    if date.year == 2016:
+        # Set the dictionary key to the month and append the location (fifth element) to the values list
+        locations_by_month[date.month].append(row[4])
+    
+# Print the dictionary
+print(locations_by_month)
