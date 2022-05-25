f77e5f9ae413005a3f1ee8e2b68820116a1f3dfa
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-25 18:32:32 +0500

    Create Exercise1.py

---
+++ Career Tracks/Python Programmer/Data Manipulation with pandas/Transforming DataFrames/Exercise1.py
@@ -0,0 +1,18 @@
+"""
+1> Print the head of the "homelessness" DataFrame.
+2> Print information about the column types and missing values in homelessness.
+3> Print the number of rows and columns in homelessness
+4> Print some summary statistics that describe the homelessness DataFrame.
+"""
+
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
