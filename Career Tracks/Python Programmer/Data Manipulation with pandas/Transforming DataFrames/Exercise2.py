15762cce1e81d3f6106e7765a4709c29963da414
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-25 18:35:09 +0500

    Create Exercise2.py

---
+++ Career Tracks/Python Programmer/Data Manipulation with pandas/Transforming DataFrames/Exercise2.py
@@ -0,0 +1,18 @@
+"""
+1> Import pandas using the alias pd.
+2> Print a 2D NumPy array of the values in homelessness.
+3> Print the column names of homelessness.
+4> Print the index of homelessness.
+"""
+
+# Import pandas using the alias pd
+import pandas as pd
+
+# Print the values of homelessness
+print(homelessness.values)
+
+# Print the column index of homelessness
+print(homelessness.columns)
+
+# Print the row index of homelessness
+print(homelessness.index)
