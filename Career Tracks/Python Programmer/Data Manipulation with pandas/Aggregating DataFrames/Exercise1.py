854c0b6d973f988c1aca879d9d692c6b390d0b2a
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-26 17:17:11 +0500

    Update Exercise1.py

--- Career Tracks/Python Programmer/Data Manipulation with pandas/Aggregating DataFrames/Exercise1.py
+++ Career Tracks/Python Programmer/Data Manipulation with pandas/Aggregating DataFrames/Exercise1.py
@@ -1,1 +1,17 @@
+"""
+1> Explore your new DataFrame first by printing the first few rows of the sales DataFrame.
+2> Print information about the columns in sales.
+3> Print the mean of the weekly_sales column.
+4> Print the median of the weekly_sales column.
+"""
+# Print the head of the sales DataFrame
+print(sales.head())
 
+# Print the info about the sales DataFrame
+print(sales.info())
+
+# Print the mean of weekly_sales
+print(sales['weekly_sales'].mean())
+
+# Print the median of weekly_sales
+print(sales['weekly_sales'].median())
