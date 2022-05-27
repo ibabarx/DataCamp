3f35ba937b28b09ce198e7919ad23232affa84ec
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-27 14:13:18 +0500

    Create Exercise5.py

---
+++ Career Tracks/Python Programmer/Data Manipulation with pandas/Slicing and Indexing DataFrames/Exercise5.py
@@ -0,0 +1,18 @@
+"""
+1> Sort the index of temperatures_ind.
+2> Use slicing with .loc[] to get these subsets:
+    from Pakistan to Russia.
+    from Lahore to Moscow. (This will return nonsense.)
+    from Pakistan, Lahore to Russia, Moscow.
+"""
+# Sort the index of temperatures_ind
+temperatures_srt = temperatures_ind.sort_index()
+
+# Subset rows from Pakistan to Russia
+print(temperatures_srt.loc['Pakistan':'Russia'])
+
+# Try to subset rows from Lahore to Moscow
+print(temperatures_srt.loc['Lahore':'Moscow'])
+
+# Subset rows from Pakistan, Lahore to Russia, Moscow
+print(temperatures_srt.loc[('Pakistan','Lahore'):('Russia','Moscow')])
