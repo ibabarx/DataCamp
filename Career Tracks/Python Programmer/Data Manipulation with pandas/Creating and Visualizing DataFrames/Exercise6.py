c7086c61a441f55bc60d6efb441b327e0ef2a493
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-27 19:58:42 +0500

    Create Exercise6.py

---
+++ Career Tracks/Python Programmer/Data Manipulation with pandas/Creating and Visualizing DataFrames/Exercise6.py
@@ -0,0 +1,10 @@
+"""
+1> Remove the rows of avocados_2016 that contain missing values and store the remaining rows in avocados_complete.
+2> Verify that all missing values have been removed from avocados_complete. Calculate each column that has NAs and print.
+"""
+
+# Remove rows with missing values
+avocados_complete = avocados_2016.dropna()
+
+# Check if any columns contain missing values
+print(avocados_complete.isna().any())
