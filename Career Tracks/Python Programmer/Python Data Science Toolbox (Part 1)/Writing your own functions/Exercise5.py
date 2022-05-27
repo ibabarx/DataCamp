94d89fee63d2984e8d8f3e96940fedd02afdba5b
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-27 21:21:44 +0500

    Create Exercise5.py

---
+++ Career Tracks/Python Programmer/Python Data Science Toolbox (Part 1)/Writing your own functions/Exercise5.py
@@ -0,0 +1,10 @@
+"""
+1> Unpack nums to the variables num1, num2, and num3.
+2> Construct a new tuple, even_nums composed of the same elements in nums, but with the 1st element replaced with the value, 2.
+"""
+
+# Unpack nums into num1, num2, and num3
+num1,num2,num3 = nums
+# Construct even_nums
+even_nums = (2,4,6)
+
