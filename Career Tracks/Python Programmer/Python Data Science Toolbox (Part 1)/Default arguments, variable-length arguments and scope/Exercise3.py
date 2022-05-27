057d4bde0f5a1d4e21576ac7a28cf7e411b68395
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-27 21:36:55 +0500

    Create Exercise3.py

---
+++ Career Tracks/Python Programmer/Python Data Science Toolbox (Part 1)/Default arguments, variable-length arguments and scope/Exercise3.py
@@ -0,0 +1,29 @@
+"""
+1> Complete the function header of the inner function with the function name inner_echo() and a single parameter word1.
+2> Complete the function echo() so that it returns inner_echo.
+3> We have called echo(), passing 2 as an argument, and assigned the resulting function to twice. Your job is to call echo(), passing 3 as an argument. Assign the 
+   resulting function to thrice.
+4> Hit Submit to call twice() and thrice() and print the results.
+"""
+
+# Define echo
+def echo(n):
+    """Return the inner_echo function."""
+
+    # Define inner_echo
+    def inner_echo(word1):
+        """Concatenate n copies of word1."""
+        echo_word = word1 * n
+        return echo_word
+
+    # Return inner_echo
+    return inner_echo
+
+# Call echo: twice
+twice = echo(2)
+
+# Call echo: thrice
+thrice = echo(3)
+
+# Call twice() and thrice() then print
+print(twice('hello'), thrice('hello'))
