67c3fa2a89f8704d996a2d110a9a42de922d2ffa
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-28 13:44:26 +0500

    Create Exercise9.py

---
+++ Career Tracks/Python Programmer/Python Data Science Toolbox (Part 2)/List comprehensions and generators/Exercise9.py
@@ -0,0 +1,15 @@
+"""
+1> Extract the column 'created_at' from df and assign the result to tweet_time. Fun fact: the extracted column in tweet_time here is a Series data structure!
+2> Create a list comprehension that extracts the time from each row in tweet_time. Each row is a string that represents a timestamp, and you will access the 
+   12th to 19th characters in the string to extract the time. Use entry as the iterator variable and assign the result to tweet_clock_time. 
+   Remember that Python uses 0-based indexing!
+"""
+
+# Extract the created_at column from df: tweet_time
+tweet_time = df['created_at']
+
+# Extract the clock time: tweet_clock_time
+tweet_clock_time = [entry[11:19] for entry in tweet_time]
+
+# Print the extracted times
+print(tweet_clock_time)
