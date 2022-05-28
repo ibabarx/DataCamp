6ca11a12310f728bb6111e8bb5760dc6c1ef0515
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-28 11:09:29 +0500

    Create Exercise3.py

---
+++ Career Tracks/Python Programmer/Python Data Science Toolbox (Part 2)/Using iterators in PythonLand/Exercise3.py
@@ -0,0 +1,23 @@
+"""
+1> Create a range object that would produce the values from 10 to 20 using range(). Assign the result to values.
+2> Use the list() function to create a list of values from the range object values. Assign the result to values_list.
+3> Use the sum() function to get the sum of the values from 10 to 20 from the range object values. Assign the result to values_sum
+"""
+
+# Create a range object: values
+values = range(10,21)
+
+# Print the range object
+print(values)
+
+# Create a list of integers: values_list
+values_list = list(values)
+
+# Print values_list
+print(values_list)
+
+# Get the sum of values: values_sum
+values_sum = sum(values)
+
+# Print values_sum
+print(values_sum)
