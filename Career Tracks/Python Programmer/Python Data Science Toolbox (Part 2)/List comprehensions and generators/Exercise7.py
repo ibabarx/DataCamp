1ba605bfb2c13fb09231ae70debe56da75c69b87
ibabarx <104021733+ibabarx@users.noreply.github.com>
2022-05-28 13:09:19 +0500

    Create Exercise7.py

---
+++ Career Tracks/Python Programmer/Python Data Science Toolbox (Part 2)/List comprehensions and generators/Exercise7.py
@@ -0,0 +1,14 @@
+"""
+1> Write a generator expression that will generate the lengths of each string in lannister. Use person as the iterator variable. Assign the result to lengths.
+2> Supply the correct iterable in the for loop for printing the values in the generator object.
+"""
+
+# Create a list of strings: lannister
+lannister = ['cersei', 'jaime', 'tywin', 'tyrion', 'joffrey']
+
+# Create a generator object: lengths
+lengths = (len(person) for person in lannister)
+
+# Iterate over and print the values in lengths
+for value in lengths:
+    print(value)
