8cf4cf75ac60f45fd687a43b1dc986e74e2a4bf7
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-06-14 19:08:34 +0500

    Create SubsettingPivotTables.py

---
+++ Career Tracks/Data Analyst/Data Manipulation with pandas/Slicing and Indexing DataFrames/SubsettingPivotTables.py
@@ -0,0 +1,15 @@
+"""
+Use .loc[] on temp_by_country_city_vs_year to take subsets.
+  From Egypt to India.
+  From Egypt, Cairo to India, Delhi.
+  From Egypt, Cairo to India, Delhi, and 2005 to 2010.
+"""
+
+# Subset for Egypt to India
+print(temp_by_country_city_vs_year.loc['Egypt':'India'])
+
+# Subset for Egypt, Cairo to India, Delhi
+print(temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi')])
+
+# Subset for Egypt, Cairo to India, Delhi, and 2005 to 2010
+print(temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi'),'2005':'2010'])
