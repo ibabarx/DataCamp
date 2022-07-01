fb8e371fffe37adadc8f3c04843286b66323a741
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-01 17:24:23 +0500

    Create AddingATitleToFacetGridObject.py

---
+++ Career Tracks/Data Analyst/Introduction to Data Visualization with Seaborn/Customizing Seaborn Plots/AddingATitleToFacetGridObject.py
@@ -0,0 +1,16 @@
+"""
+Add the following title to this plot: "Car Weight vs. Horsepower".
+"""
+
+# Create scatter plot
+g = sns.relplot(x="weight", 
+                y="horsepower", 
+                data=mpg,
+                kind="scatter")
+
+# Add a title "Car Weight vs. Horsepower"
+
+g.fig.suptitle('Car Weight vs. Horsepower')
+
+# Show plot
+plt.show()
