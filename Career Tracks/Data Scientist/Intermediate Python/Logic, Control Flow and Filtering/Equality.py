d512d47a782326b292bd3628f9087aa930f8ab2f
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-02 13:49:44 +0500

    Create Equality.py

---
+++ Career Tracks/Data Scientist/Intermediate Python/Logic, Control Flow and Filtering/Equality.py
@@ -0,0 +1,20 @@
+"""
+
+    1. In the editor on the right, write code to see if True equals False.
+    2. Write Python code to check if -5 * 15 is not equal to 75.
+    3. Ask Python whether the strings "pyscript" and "PyScript" are equal.
+    4. What happens if you compare booleans and integers? Write code to see if True and 1 are equal.
+
+"""
+
+# Comparison of booleans
+True == False
+
+# Comparison of integers
+(-5 * 15) != 75
+
+# Comparison of strings
+'pyscript' == 'PyScript'
+
+# Compare a boolean with an integer
+True == 1
