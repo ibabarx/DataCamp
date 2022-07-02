da937f89fd561cb38f6bcad8f5c97f1728db112c
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-02 13:51:26 +0500

    Create GreaterAndLessThan.py

---
+++ Career Tracks/Data Scientist/Intermediate Python/Logic, Control Flow and Filtering/GreaterAndLessThan.py
@@ -0,0 +1,19 @@
+"""
+
+    Write Python expressions, wrapped in a print() function, to check whether:
+        1. x is greater than or equal to -10. x has already been defined for you.
+        2. "test" is less than or equal to y. y has already been defined for you.
+        3. True is greater than False.
+
+"""
+
+# Comparison of integers
+x = -3 * 6
+print(x >= -10)
+
+# Comparison of strings
+y = "test"
+print('test' <= y)
+
+# Comparison of booleans
+True > False
