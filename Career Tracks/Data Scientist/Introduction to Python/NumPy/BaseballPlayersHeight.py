63261438f17cf90d0fa1e54686c2ca54f567145e
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-02 09:53:35 +0500

    Create BaseballPlayersHeight.py

---
+++ Career Tracks/Data Scientist/Introduction to Python/NumPy/BaseballPlayersHeight.py
@@ -0,0 +1,23 @@
+"""
+1 > Create a numpy array from height_in. Name this new array np_height_in.
+2 > Print np_height_in.
+3 > Multiply np_height_in with 0.0254 to convert all height measurements from inches to meters. Store the new values in a new array, np_height_m.
+4 > Print out np_height_m and check if the output makes sense.
+"""
+
+# height_in is available as a regular list
+
+# Import numpy
+import numpy as np
+
+# Create a numpy array from height_in: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height_in
+print(np_height_in)
+
+# Convert np_height_in to m: np_height_m
+np_height_m = np_height_in * 0.0254
+
+# Print np_height_m
+print(np_height_m)
