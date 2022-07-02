c5f7fe05dbdd5c439424018ae41435e5f0a5573b
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-02 10:47:46 +0500

    Create VariableAssignment.py

---
+++ Career Tracks/Data Scientist/Introduction to Python/Python Basics/VariableAssignment.py
@@ -0,0 +1,10 @@
+"""
+1 > Create a variable savings with the value 100.
+2 > Check out this variable by typing print(savings) in the script.
+"""
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
