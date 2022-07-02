77f2da283caba7fb617d06043f41991777eab074
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-02 13:07:27 +0500

    Create AccessDictionary.py

---
+++ Career Tracks/Data Scientist/Intermediate Python/Dictionaries & Pandas/AccessDictionary.py
@@ -0,0 +1,15 @@
+"""
+
+    1. Check out which keys are in europe by calling the keys() method on europe. Print out the result.
+    2. Print out the value that belongs to the key 'norway'.
+
+"""
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
