6d3405ffe79e36612109c8bc467bf8e16b5c96e0
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-02 13:26:32 +0500

    Create DictionaryToDataFrame2.py

---
+++ Career Tracks/Data Scientist/Intermediate Python/Dictionaries & Pandas/DictionaryToDataFrame2.py
@@ -0,0 +1,26 @@
+"""
+
+    1. Hit Run Code to see that, indeed, the row labels are not correctly set.
+    2. Specify the row labels by setting cars.index equal to row_labels.
+    3. Print out cars again and check if the row labels are correct this time.
+
+"""
+
+import pandas as pd
+
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+cars_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(cars_dict)
+print(cars)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index = row_labels
+
+# Print cars again
+print(cars)
