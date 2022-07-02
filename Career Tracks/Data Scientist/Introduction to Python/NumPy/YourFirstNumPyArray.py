7e6ae0413f3d72213c48511f50329ef796f107d2
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-02 09:50:43 +0500

    Create YourFirstNumPyArray.py

---
+++ Career Tracks/Data Scientist/Introduction to Python/NumPy/YourFirstNumPyArray.py
@@ -0,0 +1,17 @@
+"""
+1 > Import the numpy package as np, so that you can refer to numpy with np.
+2 > Use np.array() to create a numpy array from baseball. Name this array np_baseball.
+3 > Print out the type of np_baseball to check that you got it right.
+"""
+
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
