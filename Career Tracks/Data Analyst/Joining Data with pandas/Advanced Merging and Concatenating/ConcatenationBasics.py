d351bd804e91a2175cc7fc99136ad751d1a5aa74
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-07 15:21:21 +0500

    Create ConcatenationBasics.py

---
+++ Career Tracks/Data Analyst/Joining Data with pandas/Advanced Merging and Concatenating/ConcatenationBasics.py
@@ -0,0 +1,24 @@
+"""
+1 > Concatenate tracks_master, tracks_ride, and tracks_st, in that order, setting sort to True.
+2 > Concatenate tracks_master, tracks_ride, and tracks_st, where the index goes from 0 to n-1.
+3 > Concatenate tracks_master, tracks_ride, and tracks_st, showing only columns that are in all tables.
+"""
+
+# Concatenate the tracks
+tracks_from_albums = pd.concat([tracks_master,tracks_ride,tracks_st],
+                               sort=True)
+print(tracks_from_albums)
+
+
+# Concatenate the tracks so the index goes from 0 to n-1
+tracks_from_albums = pd.concat([tracks_master,tracks_ride,tracks_st],
+                               ignore_index=True,
+                               sort=True)
+print(tracks_from_albums)
+
+
+# Concatenate the tracks, show only columns names that are in all tables
+tracks_from_albums = pd.concat([tracks_master,tracks_ride,tracks_st],
+                               join='inner',
+                               sort=True)
+print(tracks_from_albums)
