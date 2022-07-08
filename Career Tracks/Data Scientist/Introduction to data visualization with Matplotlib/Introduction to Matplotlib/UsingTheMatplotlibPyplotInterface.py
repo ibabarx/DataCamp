3c1cd47fedf29900e4502ff4ce34ca5e722cc0fa
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-08 16:44:00 +0500

    Create UsingTheMatplotlibPyplotInterface.py

---
+++ Career Tracks/Data Scientist/Introduction to data visualization with Matplotlib/Introduction to Matplotlib/UsingTheMatplotlibPyplotInterface.py
@@ -0,0 +1,16 @@
+"""
+
+    Import the matplotlib.pyplot API, using the conventional name plt.
+    Create Figure and Axes objects using the plt.subplots function.
+    Show the results, an empty set of axes, using the plt.show function.
+
+"""
+
+# Import the matplotlib.pyplot submodule and name it plt
+import matplotlib.pyplot as plt
+
+# Create a Figure and an Axes with plt.subplots
+fig,ax = plt.subplots()
+
+# Call the show function to show the result
+plt.show()
