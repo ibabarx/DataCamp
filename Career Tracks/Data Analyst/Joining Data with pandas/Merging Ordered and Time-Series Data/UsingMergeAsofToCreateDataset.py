3dd185b94cd9ef5b6dd76006ee942bb605e54d19
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-08 09:58:24 +0500

    Create UsingMergeAsofToCreateDataset.py

---
+++ Career Tracks/Data Analyst/Joining Data with pandas/Merging Ordered and Time-Series Data/UsingMergeAsofToCreateDataset.py
@@ -0,0 +1,16 @@
+"""
+1 > Using merge_asof(), merge gdp and recession on date, with gdp as the left table. Save to the variable gdp_recession.
+2 > Create a list using a list comprehension and a conditional expression, named is_recession, where for each row if the gdp_recession['econ_status'] value is equal to 
+    'recession' then enter 'r' else 'g'.
+3 > Using gdp_recession, plot a bar chart of gdp versus date, setting the color argument equal to is_recession.
+"""
+
+# Merge gdp and recession on date using merge_asof()
+gdp_recession = pd.merge_asof(gdp,recession,on='date')
+
+# Create a list based on the row value of gdp_recession['econ_status']
+is_recession = ['r' if s=='recession' else 'g' for s in gdp_recession['econ_status']]
+
+# Plot a bar chart of gdp_recession
+gdp_recession.plot(kind='bar', y='gdp', x='date', color=is_recession, rot=90)
+plt.show()
