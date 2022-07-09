8dba857bf4d6c637b6b1a4c468d37d510407817f
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-09 22:07:52 +0500

    Create YourFirstNumPyArray.py

---
+++ Career Tracks/Data Scientist/Introduction to NumPy/Understanding NumPy Arrays/YourFirstNumPyArray.py
@@ -0,0 +1,17 @@
+"""
+
+
+    Import NumPy using its generally accepted alias.
+    Convert sudoku_list into a NumPy array called sudoku_array.
+    Print the class type() of sudoku_array to check that your code has worked properly.
+
+"""
+
+# Import NumPy
+import numpy as np
+
+# Convert sudoku_list into an array
+sudoku_array = np.array(sudoku_list)
+
+# Print the type of sudoku_array 
+print(type(sudoku_array))
