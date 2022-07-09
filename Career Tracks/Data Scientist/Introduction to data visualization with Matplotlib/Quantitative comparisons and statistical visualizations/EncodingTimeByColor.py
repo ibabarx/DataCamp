829b1370f16b28ec882e8476ce0ba35e436ea30c
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-09 21:10:06 +0500

    Create EncodingTimeByColor.py

---
+++ Career Tracks/Data Scientist/Introduction to data visualization with Matplotlib/Quantitative comparisons and statistical visualizations/EncodingTimeByColor.py
@@ -0,0 +1,18 @@
+"""
+Using the ax.scatter method add a scatter plot of the "co2" column (x-axis) against the "relative_temp" column.
+Use the c key-word argument to pass in the index of the DataFrame as input to color each point according to its date.
+Set the x-axis label to "CO2 (ppm)" and the y-axis label to "Relative temperature (C)".
+"""
+
+fig, ax = plt.subplots()
+
+# Add data: "co2", "relative_temp" as x-y, index as color
+ax.scatter(climate_change['co2'],climate_change['relative_temp'],c=climate_change.index)
+
+# Set the x-axis label to "CO2 (ppm)"
+ax.set_xlabel("CO2 (ppm)")
+
+# Set the y-axis label to "Relative temperature (C)"
+ax.set_ylabel("Relative temperature (C)")
+
+plt.show()
