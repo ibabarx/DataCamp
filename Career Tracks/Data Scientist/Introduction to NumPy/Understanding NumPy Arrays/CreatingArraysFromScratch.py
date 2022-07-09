a72be30f7df9f23f3387e7ccf5c55df9804d6b02
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-09 22:10:21 +0500

    Create CreatingArraysFromScratch.py

---
+++ Career Tracks/Data Scientist/Introduction to NumPy/Understanding NumPy Arrays/CreatingArraysFromScratch.py
@@ -0,0 +1,12 @@
+"""
+1. Create and print an array filled with zeros called zero_array, which has two rows and four columns.
+2. Create and print an array of random floats between 0 and 1 called random_array, which has three rows and six columns.
+"""
+
+# Create an array of zeros which has four columns and two rows
+zero_array = np.zeros((2,4))
+print(zero_array)
+
+# Create an array of random floats which has six columns and three rows
+random_array = np.random.random((3,6))
+print(random_array)
