ba4d0e061a29e77afe536975cb7c1c33bee8fe0c
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2022-07-09 21:08:02 +0500

    Create SimpleScatterPlot.py

---
+++ Career Tracks/Data Scientist/Introduction to data visualization with Matplotlib/Quantitative comparisons and statistical visualizations/SimpleScatterPlot.py
@@ -0,0 +1,18 @@
+"""
+Using the ax.scatter method, add the data to the plot: "co2" on the x-axis and "relative_temp" on the y-axis.
+Set the x-axis label to "CO2 (ppm)".
+Set the y-axis label to "Relative temperature (C)".
+"""
+
+fig, ax = plt.subplots()
+
+# Add data: "co2" on x-axis, "relative_temp" on y-axis
+ax.scatter(climate_change['co2'],climate_change['relative_temp'])
+
+# Set the x-axis label to "CO2 (ppm)"
+ax.set_xlabel('CO2 (ppm)')
+
+# Set the y-axis label to "Relative temperature (C)"
+ax.set_ylabel('Relative temperature (C)')
+
+plt.show()
